1827e377c3939a417bfa5ac53b694cf40ca26844
TaraTabet <tara.tabet@mail.mcgill.ca>
2018-02-28 19:35:33 -0500

    Add files via upload

--- cache.vhd
+++ cache.vhd
@@ -17,6 +17,10 @@ port(
 	s_write : in std_logic;
 	s_writedata : in std_logic_vector (31 downto 0);
 	s_waitrequest : out std_logic; 
+
+	state_thing : out integer;
+  	send_data_packet_thing : out integer;
+    	read_data_packet_thing : out integer;
     
 	m_addr : out integer range 0 to ram_size-1;
 	m_read : out std_logic;
@@ -60,33 +64,43 @@ signal read_waitrequest: std_logic := '1';
 signal write_waitrequest: std_logic := '1';
 signal i : INTEGER := 0;
 signal cache_struct: cache_struct_is;
+signal send_data_packet : integer := 0;
+signal read_data_packet : integer := 0;
+signal send_data_counter : integer := 0;
+signal read_data_counter : integer := 0;
+signal send_temp_address : integer := 0;
+signal read_temp_address : integer := 0;
+signal mem_addr_from_cache : STD_LOGIC_VECTOR (14 downto 0);
 
 begin
 
 -- make circuits here
 cache_FSM_do: process(clock, s_write, s_read, reset)
 	variable index: INTEGER;
-	
+
 begin
 	index := to_integer(unsigned(s_addr(8 downto 4)));
 	
-	if (reset = '0') then -- Check for reset
+	if (reset = '1') then -- Check for reset
 		state <= RESET_CACHE;
 	elsif (rising_edge(clock)) THEN -- If not reset, do...
 		case state is
 			when RESET_CACHE => -- Reset validity & dirty bits for each cache block
+				state_thing<=0;
 				for i in 0 to 31 loop
 					cache_struct(i).valid_bit <= '0';
 					cache_struct(i).dirty_bit <= '0';
 				end loop;
 				state <= WAITING;
 			when WAITING => -- wait for when cache is called upon
+				state_thing<=1;
 				read_waitrequest <= '1';
 				write_waitrequest <= '1';
 				if (s_read = '1' or s_write = '1' ) then
 					state <= CHECK_TAG_VALID;
 				end if;
 			when CHECK_TAG_VALID => -- checks for the address' validity in the intended index
+				state_thing<=2;
 				-- if valid and tag is a match: hit
 				if (cache_struct(index).valid_bit = '1' and cache_struct(index).tag = s_addr(14 downto 9)) then
 					state <= HIT;
@@ -94,33 +108,116 @@ begin
 					state <= MISS;
 				end if;
 			when HIT => -- data in cache, reading or writing?
+				state_thing<=3;
 				if (s_read = '1' and s_write = '0') then
 					state <= CACHE_READ;
 				elsif (s_read = '0' and s_write = '1') then
 					state <= CACHE_WRITE;
 				end if;
 			when MISS => -- check if the data in cache is dirty
+				state_thing<=4;
+				send_data_packet <= 0;
+		  		read_data_packet <= 0;
+				send_data_counter <= 0;
+		  		read_data_counter <= 0;
 				if (cache_struct(index).dirty_bit = '0') then
 					state <= NOT_DIRTY;
 				elsif (cache_struct(index).dirty_bit = '1') then
 					state <= DIRTY;
 				end if;
 			when CACHE_READ =>
+				state_thing<=5;
 				-- get the data from the block array[s_addr(3 downto 2)]
 				s_readdata <= cache_struct(index).cache_data(to_integer(unsigned(s_addr(3 downto 2))));
 				read_waitrequest <= '0';
 				state <= WAITING;
 			when CACHE_WRITE =>
+				state_thing<=6;
 				-- write the data to the block array[s_addr(3 downto 2)]
 				cache_struct(index).cache_data(to_integer(unsigned(s_addr(3 downto 2)))) <= s_writedata;
 				write_waitrequest <= '0';
 				state <= WAITING;
 			when NOT_DIRTY =>
+				state_thing<=7;
 				state <= READ_MM;
 			when DIRTY =>
+				state_thing<=8;
 				state <= WRITE_MM;
 			when READ_MM =>
+				state_thing<=9;
+				m_read <= '0';
+				m_write <= '0';
+				
+				if((read_data_counter=0) and (read_data_packet = 0)) then
+					mem_addr_from_cache <= s_addr (14 downto 4)&"0000";
+					read_temp_address <= to_integer(unsigned(mem_addr_from_cache));
+					cache_struct(index).cache_data(0)(7 downto 0) <= m_readdata;
+					m_addr <= read_temp_address;
+					read_data_counter <= read_data_counter + 1;
+					m_read <= '1';
+				end if;
+
+				if(m_waitrequest='0') then				
+				
+					mem_addr_from_cache <= s_addr (14 downto 4)&"0000";	
+
+					if(read_data_packet < 4) then
+					
+						read_temp_address <= to_integer(unsigned(mem_addr_from_cache)) + read_data_packet*4 +read_data_counter;
+
+						
+
+						m_addr <= read_temp_address;
+						cache_struct(index).cache_data(read_data_packet)(7+read_data_counter*8 downto read_data_counter*8) <= m_readdata;
+						read_data_counter <= read_data_counter + 1;
+						m_read <= '1';
+						if(read_data_counter > 3) then
+							read_data_packet <= read_data_packet + 1;
+							read_data_counter <= 0;
+						end if;
+					end if;
+				end if;		
+				if((read_data_counter*read_data_packet) = 16) then
+					state<=HIT;
+				end if;
+
 			when WRITE_MM =>
+				state_thing<=10;
+				m_read <= '0';
+				m_write <= '0';
+				
+				if((send_data_counter=0) and (send_data_packet = 0)) then
+					mem_addr_from_cache <= s_addr (14 downto 4)&"0000";
+					send_temp_address <= to_integer(unsigned(mem_addr_from_cache));
+					m_writedata <= cache_struct(index).cache_data(0)(7 downto 0);
+					m_addr <= send_temp_address;
+					send_data_counter <= send_data_counter + 1;
+					m_write <= '1';
+				end if;
+
+				if(m_waitrequest='0') then					
+				
+					mem_addr_from_cache <= s_addr (14 downto 4)&"0000";	
+
+					if(send_data_packet < 4) then
+					
+						send_temp_address <= to_integer(unsigned(mem_addr_from_cache)) + send_data_packet*4 +send_data_counter;
+					
+						
+						m_addr <= send_temp_address;
+						m_writedata <= cache_struct(index).cache_data(send_data_packet)(7+send_data_counter*8 downto send_data_counter*8);
+						send_data_counter <= send_data_counter + 1;
+						m_write <= '1';
+						if(send_data_counter > 3) then
+							send_data_packet <= send_data_packet + 1;
+							send_data_counter <= 0;
+						end if;
+					end if;
+				end if;
+				if((read_data_counter*read_data_packet) = 16) then
+					state<=READ_MM;
+				end if;
+
 		end case;
 	end if;
 			
--- cache_tb.vhd
+++ cache_tb.vhd
@@ -9,7 +9,7 @@ architecture behavior of cache_tb is
 
 component cache is
 generic(
-    ram_size : INTEGER := 32768;
+    ram_size : INTEGER := 32768
 );
 port(
     clock : in std_logic;
@@ -22,6 +22,10 @@ port(
     s_write : in std_logic;
     s_writedata : in std_logic_vector (31 downto 0);
     s_waitrequest : out std_logic; 
+    
+    state_thing : out integer;
+    send_data_packet_thing : out integer;
+    read_data_packet_thing : out integer;
 
     m_addr : out integer range 0 to ram_size-1;
     m_read : out std_logic;
@@ -61,6 +65,10 @@ signal s_write : std_logic;
 signal s_writedata : std_logic_vector (31 downto 0);
 signal s_waitrequest : std_logic;
 
+signal state_thing : integer;
+signal send_data_packet_thing : integer;
+signal read_data_packet_thing : integer;
+
 signal m_addr : integer range 0 to 2147483647;
 signal m_read : std_logic;
 signal m_readdata : std_logic_vector (7 downto 0);
@@ -83,6 +91,10 @@ port map(
     s_write => s_write,
     s_writedata => s_writedata,
     s_waitrequest => s_waitrequest,
+
+    state_thing => state_thing,
+    send_data_packet_thing => send_data_packet_thing,
+    read_data_packet_thing => read_data_packet_thing,
 
     m_addr => m_addr,
     m_read => m_read,
@@ -114,8 +126,53 @@ end process;
 
 test_process : process
 begin
-
+    
 -- put your tests here
+	
+	--We will test for different combinations of {valid/invalid block, 
+  --dirty/not dirty block, access is read/write, tag is equal/tag is not equal}
+  --  
+  --There are 16 test cases:
+    -- #1:  {Valid, Dirty, Read, Tag Equal}
+    -- #2:  {Valid, Dirty, Read, Tag Not Equal}
+    -- #3:  {Valid, Dirty, Write, Tag Equal}
+    -- #4:  {Valid, Dirty, Write, Tag Not Equal}
+    -- #5:  {Valid, Not Dirty, Read, Tag Equal}
+    -- #6:  {Valid, Not Dirty, Read, Tag Not Equal}
+    -- #7:  {Valid, Not Dirty, Write, Tag Equal}
+    -- #8:  {Valid, Not Dirty, Write, Tag Not Equal}
+    -- #9:  {Invalid, Dirty, Read, Tag Equal}
+    -- #10: {Invalid, Dirty, Read, Tag Not Equal}
+    -- #11: {Invalid, Dirty, Write, Tag Equal}
+    -- #12: {Invalid, Dirty, Write, Tag Not Equal}
+    -- #13: {Invalid, Not Dirty, Read, Tag Equal}
+    -- #14: {Invalid, Not Dirty, Read, Tag Not Equal}
+    -- #15: {Invalid, Not Dirty, Write, Tag Equal}
+    -- #16: {Invalid, Not Dirty, Write, Tag Not Equal}
+    
+    --Test cases 9, 10, 11 and 12 cannot be tested for since an invalid block cannot be dirty.
+	s_write <= '0';
+	s_read <= '0';
+ 	--Test Case #15
+	--writes to an empty slot with tag equal
+	s_addr <= "00000000000000000001001001001000";
+	s_writedata <= "00110101001010001101010101010111";
+	s_write <= '1';
+	WAIT FOR 20*clk_period;
+	s_write <='0';
+	WAIT FOR 1*clk_period;
+	
+	WAIT until s_waitrequest = '0';
+	WAIT FOR 1*clk_period;
+	
+	--Test Case #1
+	--reads info that has been written but not saved in memory
+	s_addr <= "00000000000000000001001001001000";
+	s_read <= '1';
+	WAIT FOR 20*clk_period;
+	s_read <='0';
+	WAIT FOR 1*clk_period;
+	reset <= '0';
 	
 end process;
 	
