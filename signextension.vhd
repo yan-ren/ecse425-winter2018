004757652f5a9f047d282cc7beb631a6f5dd3402
Yan Ren <ryan.ren3@gmail.com>
2018-03-21 23:50:41 -0400

    add register and signextension

---
+++ signextension.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity signextension is
+port(
+	bit16 : in std_logic_vector(15 downto 0);
+	bit32 : out std_logic_vector(31 downto 0)
+);
+end signextension;
+
+architecture arch of signextension is
+begin
+	bit32 <=	"0000000000000000" & bit16 when bit16(15) ='0' else
+				"1111111111111111" & bit16;
+end arch;
